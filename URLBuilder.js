7896e6c34b71ccb7ed6ec684b4d0ae58d007934f
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-17 23:07:51 -0400

    Initial commit

--- URLBuilder.js
+++ URLBuilder.js
@@ -1,33 +1,47 @@
 export class URLBuilder {
     pattern;
-    #positions = [0];
+    #tokenStream = [];
     constructor(pattern) {
         this.pattern = pattern;
         let result;
         let i = 0;
-        const positions = this.#positions;
+        const positions = this.#tokenStream;
         while ((result = reg.exec(pattern)) !== null) {
+            const beforeToken = pattern.substring(i, result.index);
             const r = result[0];
-            const key = r.substring(1);
+            const tokenKey = r.substring(1);
             i = result.index + r.length;
-            positions.push([i, key]);
+            positions.push([beforeToken, tokenKey]);
         }
+        if (i < pattern.length) {
+            positions.push([pattern.substring(i), undefined]);
+        }
+        console.log({ positions });
     }
     build(obj) {
-        let pattern = this.pattern;
         const tokens = [];
-        let i = 0;
-        let result;
-        while ((result = reg.exec(pattern)) !== null) {
-            tokens.push(pattern.substring(i, result.index));
-            const key = result[0].substring(1);
-            i = result.index + result[0].length;
-            if (key in obj) {
+        for (const tokenItem of this.#tokenStream) {
+            const [beforeToken, key] = tokenItem;
+            tokens.push(beforeToken);
+            if (key !== undefined && key in obj) {
                 tokens.push(obj[key]);
             }
         }
-        const r = tokens.join('');
-        return r;
+        return tokens.join('');
+        // let pattern = this.pattern;
+        // const tokens = [];
+        // let i = 0;
+        // let result;
+        // while ((result = reg.exec(pattern)) !== null) {
+        //     tokens.push(pattern.substring(i, result.index));
+        //     const key = result[0].substring(1);
+        //     i = result.index + result[0].length;
+        //     if(key in obj){
+        //         tokens.push(obj[key]);
+        //     }
+        // }
+        // const r = tokens.join('');
+        // return r;
     }
 }
 const reg = /:\w+/g;
--- URLBuilder.ts
+++ URLBuilder.ts
@@ -1,32 +1,48 @@
+type BeforeToken = string;
+type TokenKey = string | undefined
 export class URLBuilder{
-    #positions: Array<[number, string]> = [0];
+    #tokenStream: Array<[BeforeToken, TokenKey]> = [];
     constructor(public pattern: string){
         let result;
         let i = 0;
-        const positions = this.#positions;
+        const positions = this.#tokenStream;
         while ((result = reg.exec(pattern)) !== null) {
+            const beforeToken = pattern.substring(i, result.index);
             const r = result[0]
-            const key = r.substring(1);
+            const tokenKey = r.substring(1);
             i = result.index + r.length;
-            positions.push([i, key]);
+            positions.push([beforeToken, tokenKey]);
         }
+        if(i < pattern.length){
+            positions.push([pattern.substring(i), undefined]);
+        }
+        console.log({positions})
     }
 
     build(obj: any){
-        let pattern = this.pattern;
         const tokens = [];
-        let i = 0;
-        let result;
-        while ((result = reg.exec(pattern)) !== null) {
-            tokens.push(pattern.substring(i, result.index));
-            const key = result[0].substring(1);
-            i = result.index + result[0].length;
-            if(key in obj){
+        for(const tokenItem of this.#tokenStream){
+            const [beforeToken, key] = tokenItem;
+            tokens.push(beforeToken);
+            if(key !== undefined && key in obj){
                 tokens.push(obj[key]);
             }
         }
-        const r = tokens.join('');
-        return r;
+        return tokens.join('');
+        // let pattern = this.pattern;
+        // const tokens = [];
+        // let i = 0;
+        // let result;
+        // while ((result = reg.exec(pattern)) !== null) {
+        //     tokens.push(pattern.substring(i, result.index));
+        //     const key = result[0].substring(1);
+        //     i = result.index + result[0].length;
+        //     if(key in obj){
+        //         tokens.push(obj[key]);
+        //     }
+        // }
+        // const r = tokens.join('');
+        // return r;
     }
 }
 
