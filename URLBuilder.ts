fbe119691e244ca0f9ccf89d0cf02aea542d149a
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-17 22:22:58 -0400

    Initial commit

---
+++ URLBuilder.ts
@@ -0,0 +1,27 @@
+export class URLBuilder{
+    //#reea: RegExpMatchArray | null = null;
+    constructor(public pattern: string){}
+
+    build(obj: any){
+        let returnStr = this.pattern;
+        //const reg = /:\w+/g;
+        const tokens = [];
+        let i = 0;
+        let result;
+        while ((result = reg.exec(returnStr)) !== null) {
+            tokens.push(returnStr.substring(i, result.index));
+            const key = result[0].substring(1);
+            i = result.index + result[0].length;
+            if(key in obj){
+                tokens.push(obj[key]);
+            }
+            
+            console.log({result});
+        }
+        const r = tokens.join('');
+        console.log({r, tokens});
+        return r;
+    }
+}
+
+const reg = /:\w+/g;
