e077c75c4913bfd8ee0e4c2f3d46987faa16f8a7
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-10-17 23:08:08 -0400

    Initial commit

--- URLBuilder.js
+++ URLBuilder.js
@@ -28,20 +28,6 @@ export class URLBuilder {
             }
         }
         return tokens.join('');
-        // let pattern = this.pattern;
-        // const tokens = [];
-        // let i = 0;
-        // let result;
-        // while ((result = reg.exec(pattern)) !== null) {
-        //     tokens.push(pattern.substring(i, result.index));
-        //     const key = result[0].substring(1);
-        //     i = result.index + result[0].length;
-        //     if(key in obj){
-        //         tokens.push(obj[key]);
-        //     }
-        // }
-        // const r = tokens.join('');
-        // return r;
     }
 }
 const reg = /:\w+/g;
